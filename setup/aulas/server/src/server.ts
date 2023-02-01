60688ca1c3f9fdb24593981d419784908d8bc603
Felipe Sousa <felipeluis.felpssousa@gmail.com>
2023-01-31 22:03:55 -0300

    Configuring prisma and testing database with search 'findMany' and more.

--- setup/aulas/server/src/server.ts
+++ setup/aulas/server/src/server.ts
@@ -1,13 +1,27 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
+
+/**
+ * Método HTTP: Get, Post, Put, Patch, Delete
+*/
 
 app.get('/', () => {
-    return 'Hello World!!'
+    const habits = prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
 })
 
 app.listen({
-    port:3333,
+    port: 3333,
 }).then(() => {
     console.log('HTTP Server running!')
 })

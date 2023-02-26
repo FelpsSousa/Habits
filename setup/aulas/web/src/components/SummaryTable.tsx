fff6d26f483815a5ea51cf43e5a75e9ff7953ce2
Felipe Sousa <felipeluis.felpssousa@gmail.com>
2023-02-26 15:23:40 -0300

    Problem with amount od days to fill fixed.

--- setup/aulas/web/src/components/SummaryTable.tsx
+++ setup/aulas/web/src/components/SummaryTable.tsx
@@ -14,7 +14,7 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning()
 
-const minimumSummaryDatesSize = 18 + 7 
+const minimumSummaryDatesSize = 18 * 7 
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 console.log(summaryDates)
@@ -42,9 +42,10 @@ export function SummaryTable() {
 
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
-                        <div key={i}
-                        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
-                        /> 
+                        <div 
+                            key={i}
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
+                        />
                     )
                 })}
             </div>
